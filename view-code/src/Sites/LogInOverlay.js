97dfea7230ed90f8e436ac8cb3c216883a88fc55
JaferShar <jafer.shar@gmail.com>
2023-01-24 03:21:33 +0100

    Änderungen an LoginButton Style und Kursübersicht Style

---
+++ view-code/src/Sites/LogInOverlay.js
@@ -0,0 +1,16 @@
+
+const kursifyLogo = require('./Kursify.jpg');
+
+//@param onClose a function that closes the LogInOverlay 
+function LogInOverlay({onClose}) {
+    return (
+        <div className='overlay-background'>
+        <div className="login-grid">
+                <img alt='Kursify Logo!' src={kursifyLogo}/>
+                <button className='loginButton' onClick={onClose}>Anmelden mit Google</button>
+        </div>
+        </div>
+    );
+}
+
+export default LogInOverlay;
