34b56e67cbeb503b1d44db19b0b9ce148d2add7c
JaferShar <jafer.shar@gmail.com>
2023-01-24 03:20:55 +0100

    Overlay gemacht, sodass auf Klick overlay auch verschwindet

--- view-code/src/Sites/CourseOverview.js
+++ view-code/src/Sites/CourseOverview.js
@@ -1,16 +1,28 @@
 import '../Styling/SiteStyling/CourseOverview.css';
 import '../Styling/ButtonStyling/LoginButton.css';
+import React, { useState } from 'react';
+import LogInOverlay from './LogInOverlay';
 
-const kursifyLogo = require('./Kursify.jpg');
+
+
 
 function CourseOverview() {
+    //Controls the Overlay Appearance State
+    //The state must be false so that when the CourseOverview site is reloaded the overlay reappears
+    const [isOpen, setIsOpen] = useState(false);
+
+    //As a prototype, when the "Log In" button is pressed the Overlay will disappear
+    //This behaviour will be changed when GoogleAccountManager is set up
+    const toggleOverlay = () => { 
+        setIsOpen(!isOpen);
+    }
+
     return (
         <div>
             <h1 className="header">Kursübersicht</h1>
-            <div className="login-grid">
-                <img src={kursifyLogo}/>
-                <button className='loginButton'>Log in</button>
-            </div>
+            {!isOpen && (
+            <LogInOverlay onClose={toggleOverlay}/>
+            )}
         </div>
     );
 }
