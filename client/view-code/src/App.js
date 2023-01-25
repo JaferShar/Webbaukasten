2250f183f6eaf2d472f2550f2dbd24bb797b4d41
JaferShar <jafer.shar@gmail.com>
2023-01-25 23:07:09 +0100

    Routing für die View

--- client/view-code/src/App.js
+++ client/view-code/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 // import CourseOverview from './Sites/CourseOverview' commented for the course view
 import Course from './Sites/Course'
+import { Route, Routes } from 'react-router-dom';
+import CourseOverview from './Sites/CourseOverview'
 
 
 function App() {
   return (
     <div>
-      <Course/>
+      <Routes>
+      <Route  path='/kurs-uebersicht' element={<CourseOverview />} />
+        <Route  path='/kurs' element={<Course />} />
+        
+      </Routes>
     </div>
   );
 }
