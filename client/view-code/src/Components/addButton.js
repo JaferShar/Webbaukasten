64eb1c74fbee996656515b4b138e891055cefa4c
Jasmin Parmar <uczyf@student.kit.edu>
2023-01-25 15:05:10 +0100

    created add button

---
+++ client/view-code/src/Components/addButton.js
@@ -0,0 +1,18 @@
+import {AiOutlinePlus} from "react-icons/ai";
+import '../Styling/ButtonStyling/addButton.css'
+
+
+function addButton(){
+    return (
+    
+    <div>
+        <div>
+            <AiOutlinePlus className='addButton'></AiOutlinePlus>
+        </div>
+        
+    </div>
+    
+    );
+
+}
+export default addButton;
