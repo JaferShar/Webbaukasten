931574c5ac021308e308380163c84d7082b75412
Nick Oelmann <nick.oelmann@web.de>
2023-01-27 16:35:11 +0100

    fixed linting

---
+++ backend/server/models/Course.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+
+const screenSchema = new mongoose.Schema({
+    template: {
+        type: String,
+        default: 'Welcome',
+        required: true,
+        immutable: true,
+    },
+    elements: [{type: mongoose.Schema.Types.ObjectId, ref:'Element'}]
+})
+
+const courseSchema = new mongoose.Schema({
+    accountID: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Account',
+        //required: true
+    },
+    courseName: {type: String, required: true},
+    section: {type: [{
+        sectionName: {
+            type: String,
+        },
+        StartIndex: {
+            type: Number,
+        },
+    },],
+    default: function() {
+        return [{sectionName: this.courseName, startIndex: 0}]
+    }},
+    screens: [screenSchema]
+})
+
+//create function
+module.exports = mongoose.model('Course', courseSchema)
