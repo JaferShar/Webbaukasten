8d8244e9a6ea60458f16f4177686b5b41dd6e67c
raphael39 <raphael.sutter@t-online.de>
2023-01-27 20:54:21 +0100

    implemented basic account controller methods

--- backend/routes/accountRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const {registerAccount, loginAccount, getMe} = require('../controllers/accountController')
-
-router.post('/', registerAccount)
-router.post('/login', loginAccount)
-router.get('/me', getMe)
-
-module.exports = router
--- backend/routes/courseRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const { getCourse, setCourse, getAllCourses, deleteCourse, updateCourse } = require('../controllers/courseManagerController')
-
-router.route('/').post(setCourse)
-router.route('/all').get(getAllCourses)
-router.route('/:id').get(getCourse).delete(deleteCourse).put(updateCourse)
-
-module.exports = router
