0aa221b99991bc5a72487621ba424d663e882573
JaferShar <jafer.shar@gmail.com>
2023-01-28 17:07:54 +0100

    BrowserRouter in App verschoben

--- client/view-code/src/App.js
+++ client/view-code/src/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
 // import CourseOverview from './Sites/CourseOverview' commented for the course view
 import Course from './Sites/Course'
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import CourseOverview from './Sites/CourseOverview'
 
 
 function App() {
   return (
     <div>
-      <Routes>
-      <Route  path='/kurs-uebersicht' element={<CourseOverview />} />
-        <Route  path='/kurs' element={<Course />} />
-        
-      </Routes>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/kurs-uebersicht' element={<CourseOverview />} />
+          <Route path='/kurs' element={<Course />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
