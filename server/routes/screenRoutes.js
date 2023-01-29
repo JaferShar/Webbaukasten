9432a242f486f706a285c643eb611d5272c6565a
Nick Oelmann <nick.oelmann@web.de>
2023-01-29 17:22:09 +0100

    added dependencies and update middleware

--- server/routes/screenRoutes.js
+++ server/routes/screenRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const { setScreen, setTextField } = require('../controllers/screenController')
+const { setScreen, setTextField, setPicture } = require('../controllers/screenController')
 
 router.route('/:id').post(setScreen)
-router.route('/element/:screenId').post(setTextField)
+router.route('/textfield/:screenId').post(setTextField)
+//router.route('/picture/:screenId').post(setPicture)
 
 module.exports = router
