9233645e0d9f7a1b2ebd476c94b5bb85c5c5c051
Nick Oelmann <nick.oelmann@web.de>
2023-01-30 12:32:59 +0100

    readded creating picture in case of bad merge

--- server/controllers/screenController.js
+++ server/controllers/screenController.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const asyncHandler = require('express-async-handler')
+const multer = require('multer')
 
 const Course = require('../models/Course')
 const { Screen, Picture, TextField, H5P } = require('../models/Screen');
@@ -54,7 +55,7 @@ const setTextField = asyncHandler(async (req, res) => {
         const textField = await TextField.create({text: text})
         const screen = await Screen.findById(req.params.screenId)
         screen.elements.push(textField)
-        screen.save()
+        await screen.save()
 
         res.status(201).json(screen)
     } catch (error) {
@@ -63,13 +64,25 @@ const setTextField = asyncHandler(async (req, res) => {
 })
 
 const setPicture = asyncHandler(async (req, res) => {
-    try {
-        const image = Picture.create({picType: req.header, data: req.body})
-        const screen = await Screen.findById(req.params.screenId)
-        screen.elements.push(image)
-        screen.save()
 
-        res.status(201).json(image)
+    try {
+        const upload = multer({storage: multer.memoryStorage()}).single('image')
+        upload(req, res, async (err) => {
+            if (err) {
+                res.status(400).json({error: err.message})
+            }
+            // create picture
+            const picture = new Picture({
+                data: req.file.buffer,
+                picType: req.file.mimetype
+            })
+            await picture.save()
+            // add picture to screen
+            const screen = await Screen.findById(req.params.screenId)
+            screen.elements.push(picture)
+            await screen.save()
+            res.status(201).json(screen)
+        })
     } catch (error) {
         res.status(400).json({error: error.message})
     }
