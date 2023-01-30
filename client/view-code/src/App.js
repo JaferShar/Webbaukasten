ee66e634785199f6688f1bc51caa813b842121d7
raphael39 <raphael.sutter@t-online.de>
2023-01-30 23:45:59 +0100

    google login site

--- client/view-code/src/App.js
+++ client/view-code/src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import CourseOverview from './Sites/CourseOverview'
 import Course from './Sites/Course'
-import Login from './Sites/Login'
+import SignInSide from './Sites/SignInSide'
 
 
 function App() {
@@ -13,7 +13,7 @@ function App() {
         <Routes>
           <Route path='/kurs-uebersicht' element={<CourseOverview />} />
           <Route path='/kurs' element={<Course />} />
-          <Route path='/login' element={<Login />} />
+          <Route path='/login' element={<SignInSide />} />
         </Routes>
       </BrowserRouter>
     </div>
