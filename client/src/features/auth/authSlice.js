f09e65b08ec2259917b15f9d7f91c0a1e45d9822
raphael39 <raphael.sutter@t-online.de>
2023-01-31 21:35:57 +0100

    setting up local storage for auth

---
+++ client/src/features/auth/authSlice.js
@@ -0,0 +1,33 @@
+import {createSlice, createAsyncThunk} from '@reduxjs/toolkit';
+
+//Get user from local storage
+const user = JSON.parse(localStorage.getItem('user'));
+
+const initialState = {
+    user: user ? user : null,
+    isError: false,
+    isSuccess: false,
+    isLoading: false,
+    message: '',
+}
+
+// Register user
+
+export const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: {
+        reset: (state) => {
+            state.isError = false;
+            state.isSuccess = false;
+            state.isLoading = false;
+            state.message = '';
+        }   
+    },
+    extraReducers: () => {
+        //TODO
+    }   
+})
+
+export const {reset} = authSlice.actions;
+export default authSlice.reducer;
