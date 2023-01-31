db0d84bf4f48eff4c2e7fcdf3d61a370cb21fd3d
Nick Oelmann <nick.oelmann@web.de>
2023-01-31 20:18:49 +0100

    last push on main!!! afterwards just merge

--- server/controllers/screenController.js
+++ server/controllers/screenController.js
@@ -145,6 +145,108 @@ const updateSection  = asyncHandler(async (req, res) => {
     }
 })
 
+// since here methods are not fully integrated, they are not tested
+
+
+// updateScreen function does not notices if element was deleted. May add functionallity to delete or create elements later
+const updateScreen = asyncHandler(async (req, res) => {
+    try {
+        const { elements } = req.body;
+    
+        // get screen
+        const screen = await Screen.findById(req.params.id);
+        if (!screen) {
+          return res.status(400).send({ error: 'Screen not found' });
+        }
+    
+        // iterate over elements and update them
+        for (let i = 0; i < elements.length; i++) {
+            // get single element
+            const { _id, elementType, ...rest } = elements[i];
+            let elementModel;
+            switch (elementType) {
+                case 'Picture':
+                elementModel = Picture;
+                break;
+                case 'TextField':
+                elementModel = TextField;
+                break;
+                case 'H5P':
+                elementModel = H5P;
+                break;
+                default:
+                return res.status(400).send({ error: 'Invalid element type' });
+            }
+    
+          // find element in screen
+          let element = screen.elements.id(_id);
+          if (!element) {
+            return res.status(400).send({ error: 'Element not found' });
+          }
+    
+          // update element specific properties
+          Object.assign(element, rest);
+    
+          // check if the order has changed
+          if (i !== screen.elements.indexOf(element)) {
+            screen.elements.splice(screen.elements.indexOf(element), 1);
+            screen.elements.splice(i, 0, element);
+          }
+        }
+    
+        // save screen if no errors occured and return success
+        await screen.save();
+        return res.status(200).send({ success: true });
+    } catch (error) {
+        return res.status(500).send({ error: error.message });
+    }
+})
+
+const deleteScreen = asyncHandler(async (req, res) => {
+    try {
+        // delete screen on course first
+        const course = await Course.findById(req.params.courseId)
+        if (!course.screens.id(req.params.id)) {
+            throw new Error('Nothing to do, screen not found')
+        }
+        course.screens.id(req.params.id).remove()
+        // save changes and delete screen afterwards
+        await course.save()
+        await Screen.findByIdAndDelete(req.params.id)
+        res.status(200).json(course)
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
+})
+
+const deleteSection = asyncHandler(async (req, res) => {
+    try {
+        const course = await Course.findById(req.params.id)
+        if (!course) {
+            throw new Error('Course not found')
+        }
+        course.sections.id(req.params.sectionId).remove()
+        await course.save()
+        res.status(200).json(course)
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
+})
+
+const deleteElement = asyncHandler(async (req, res) => {
+    try {
+        const screen = await Screen.findById(req.params.screenId)
+        if (!screen) {
+            throw new Error('Screen not found')
+        }
+        screen.elements.id(req.params.elementId).remove()
+        await screen.save()
+        res.status(200).json(screen)
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
+})
+
 
 module.exports = {
     setScreen,
