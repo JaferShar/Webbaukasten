bfe6f5b7ea3fc79919173451f06bcc3241dfcf8e
Nick Oelmann <nick.oelmann@web.de>
2023-01-31 22:12:07 +0100

    change Sites to Pages -_-

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 // import CourseOverview from './Sites/CourseOverview' commented for the course view
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import CourseOverview from './Sites/CourseOverview'
-import Course from './Sites/Course'
-import SignInSide from './Sites/SignInSide'
+import CourseOverview from './Pages/CourseOverview'
+import Course from './Pages/Course'
+import SignInSide from './Pages/SignInSide'
 
 
 function App() {
