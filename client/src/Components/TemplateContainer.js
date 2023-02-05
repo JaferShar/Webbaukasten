9eb87a80ee4de7ef63ba746f915d962edefca4be
LauraKer <laura@kernahan.de>
2023-02-05 01:03:52 +0100

    Beginner Page Template created

--- client/src/Components/TemplateContainer.js
+++ client/src/Components/TemplateContainer.js
@@ -1,12 +1,30 @@
-import React from 'react';
-import '../Styling/SiteStyling/TemplateContainer.css'
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Stack from '@mui/material/Stack';
 
-function TemplateContainer() {
-    return (
-        <div className="TemplateContainer">
-            Template Menu
-        </div>
-    );
+export default function TemplateContainer() {
+  return (
+    <div className="TemplateContainer">
+      Template Menu
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+        }}
+      >
+        <Stack spacing={4}>
+          <Paper elevation={3} style={{ width: '100%', height: '60px' }}>
+            Wilkommen Template
+          </Paper>
+          <Paper elevation={3} style={{ width: '100%', height: '60px' }}>
+            Standard Template
+          </Paper>
+          <Paper elevation={3} style={{ width: '100%', height: '60px' }}>
+            Ende Template
+          </Paper>
+        </Stack>
+      </Box>
+    </div>
+  );
 }
-
-export default TemplateContainer;
---
+++ client/src/Components/Test.js
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import PopUpButtonText from './PopUpButtonText';
+
+
+export default function MultilineTextFields() {
+  return (
+    <Box
+      component="form"
+      sx={{
+        '& .MuiTextField-root': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <div>
+        
+        <TextField
+          id="outlined-multiline-static"
+          label="Text editor"
+          multiline
+          rows={9}
+        />
+        
+      </div>
+      
+    </Box>
+  );
+}
