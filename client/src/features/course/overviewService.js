32c90b19e0088ff9396924cac32e6b4b169547ab
raphael39 <raphael.sutter@t-online.de>
2023-02-05 18:46:08 +0100

    set token for http auth automatically

--- client/src/features/course/overviewService.js
+++ client/src/features/course/overviewService.js
@@ -1,11 +1,11 @@
 import Axios from "axios";
 
 const API_URL = "api/course";
-const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzZGQyNmQwODUzODQ0NGJlZTE4MmVlNyIsImlhdCI6MTY3NTQzNzc3NiwiZXhwIjoxNjc4MDI5Nzc2fQ.E8hJPS8v4ZgNVG6dRG9Ym8eC_-NLzWZBKJ1qBbzZr0Y'
+const account = JSON.parse(localStorage.getItem("account"));
 
 const config = {
     headers: {
-        'Authorization': `Bearer ${token}` 
+        'Authorization': `Bearer ${account.token}` 
     }
 }
 
