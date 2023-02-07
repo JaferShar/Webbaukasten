3e0274f01a197a50e87c25605b7916ce359b97b1
Nick Oelmann <nick.oelmann@web.de>
2023-02-07 19:17:15 +0100

    added pre version of Screen Viewer

--- client/src/Components/CourseEditorComponents/Containers/ScreenViewer.js
+++ client/src/Components/CourseEditorComponents/Containers/ScreenViewer.js
@@ -1,12 +1,47 @@
-import React from 'react';
-import '../../../Styling/SiteStyling/ScreenContainer.css'
+import React, { useState, useEffect } from "react";
+import "../../../Styling/SiteStyling/ScreenViewer.css";
+import Article from '@mui/icons-material/Article';
+import {
+  Paper,
+  List,
+  ListItem,
+  ListItemAvatar,
+  Avatar,
+  ListItemText,
+  Grid,
+} from "@mui/material";
+import FolderIcon from "@mui/icons-material/Folder";
 
-function ScreenContainer() {
-    return (
-        <div className="ScreenContainer">
-            Hi.
-        </div>
-    );
+function ScreenViewer() {
+  const [selectedScreen, setSelectedScreen] = useState(0);
+  const [screens, setScreens] = useState([]);
+  var quantity = 20;
+
+  useEffect(() => {
+    const newScreens = [];
+    for (let i = 0; i < quantity; i++) {
+      newScreens.push({
+        name: "Screen " + (i + 1),
+      });
+    }
+    setScreens(newScreens);
+  }, [quantity]);
+
+  return (
+    <Grid className="ScreenViewer">
+
+        <Paper style={{ display: 'flex', justifyContent: 'center', maxHeight: '100%', maxWidth: '100%', overflow: "auto" }}>
+          <List>
+            {screens.map((screen, index) => (
+              <ListItem key={index} className="rectangle-list-item" style={{marginBottom: '30px', flexDirection: 'column'}}>
+                <Article style={{fontSize: 100}}/>
+                <ListItemText primaryTypographyProps={{variant: "body2"}} primary={index + 1} style={{marginTop: '60px'}}/>
+              </ListItem>
+            ))}
+          </List>
+        </Paper>
+    </Grid>
+  );
 }
 
-export default ScreenContainer;
+export default ScreenViewer;
--- client/src/Pages/CourseEditor.jsx
+++ client/src/Pages/CourseEditor.jsx
@@ -1,74 +1,66 @@
-import React from 'react';
-import '../Styling/SiteStyling/Course.css'
-import '../Styling/SiteStyling/TextContainer.css'
-import ScreenContainer from '../Components/CourseEditorComponents/Containers/ScreenViewer'
-import '../Styling/SiteStyling/ScreenContainer.css'
-import Buttons from '../Components/CourseEditorComponents/Buttons/Buttons.js';
-import "../Styling/ButtonStyling/saveButton.css"
-import "../Styling/ButtonStyling/homeButton.css"
-import "../Styling/ButtonStyling/addButton.css"
-import "../Styling/ButtonStyling/nextButton.css"
-import TemplateContainer from '../Components/CourseEditorComponents/Containers/TemplateContainer'
-import '../Styling/SiteStyling/TemplateContainer.css'
-import '../Styling/SiteStyling/PictureContainer.css'
-import Menu from '../Components/CourseEditorComponents/Containers/MenuContainer'
-import '../Styling/SiteStyling/Menu.css'
-import '../Styling/ButtonStyling/PopUpButtonText.css'
-import '../Styling/ButtonStyling/PopUpButtonPicture.css'
-import { useState } from 'react';
-import '../Styling/SiteStyling/ProgressBar.css'
-import '../Styling/SiteStyling/WelcomeTemplate.css'
-import BeginTemplate from '../Components/CourseEditorComponents/Templates/BeginTemplate';
-import MenuBeginTemplate from '../Components/CourseEditorComponents/Menus/ScreenMenu';
+import React from "react";
+import "../Styling/SiteStyling/Course.css";
+import "../Styling/SiteStyling/TextContainer.css";
+import ScreenViewer from "../Components/CourseEditorComponents/Containers/ScreenViewer";
+import "../Styling/SiteStyling/ScreenViewer.css";
+import Buttons from "../Components/CourseEditorComponents/Buttons/Buttons.js";
+import "../Styling/ButtonStyling/saveButton.css";
+import "../Styling/ButtonStyling/homeButton.css";
+import "../Styling/ButtonStyling/addButton.css";
+import "../Styling/ButtonStyling/nextButton.css";
+import TemplateContainer from "../Components/CourseEditorComponents/Containers/TemplateContainer";
+import "../Styling/SiteStyling/TemplateContainer.css";
+import "../Styling/SiteStyling/PictureContainer.css";
+import Menu from "../Components/CourseEditorComponents/Containers/MenuContainer";
+import "../Styling/SiteStyling/Menu.css";
+import "../Styling/ButtonStyling/PopUpButtonText.css";
+import "../Styling/ButtonStyling/PopUpButtonPicture.css";
+import { useState } from "react";
+import "../Styling/SiteStyling/ProgressBar.css";
+import "../Styling/SiteStyling/WelcomeTemplate.css";
+import BeginTemplate from "../Components/CourseEditorComponents/Templates/BeginTemplate";
+import MenuBeginTemplate from "../Components/CourseEditorComponents/Menus/ScreenMenu";
 
+const beginTemplate = (
+  <div>
+    <BeginTemplate></BeginTemplate>
+  </div>
+);
 
-
-
-
-const beginTemplate = <div>
-  <BeginTemplate>
-
-  </BeginTemplate>
-</div>;
-
-function Course () {
+function Course() {
   const [changeableTemplate, setChangeableTemplate] = useState(beginTemplate);
 
   const changeTemplate = (template) => {
     setChangeableTemplate(template);
-  }
+  };
 
   return (
-  <><div className="background">
-      <h1 Course="header">Kurs</h1>
-      
-      <div className="course">
-      <MenuBeginTemplate changeTemplate={changeTemplate}/>
+    <>
+      <div className="background">
+        <h1 Course="header">Kurs</h1>
+
+        <div className="course">
+          <MenuBeginTemplate changeTemplate={changeTemplate} />
+        </div>
+        <div className="buttons">
+          <Buttons>AiOutlineCheckSquare</Buttons>
+        </div>
+
+        {changeableTemplate}
       </div>
-      <div className='buttons'>
-        <Buttons>AiOutlineCheckSquare</Buttons>
+      <div>
+        <ScreenViewer />
       </div>
 
-      {changeableTemplate}
-      
-    </div><div>
-        <ScreenContainer>
+      <div>
+        <TemplateContainer></TemplateContainer>
+      </div>
 
-        </ScreenContainer>
-      </div>
-      
       <div>
-        <TemplateContainer>
-        </TemplateContainer>
+        <Menu></Menu>
       </div>
-      
-      <div>
-        <Menu>
-        </Menu>
-      </div>
-      </>
-      
-      )
+    </>
+  );
 }
 
 export default Course;
