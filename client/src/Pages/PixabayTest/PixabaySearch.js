4da451588dcc736dd549c6f9a5820909c2991ddf
JaferShar <jafer.shar@gmail.com>
2023-02-09 22:25:29 +0100

    Author nicht mehr in der Lage Bilderanzahl zu verändern

--- client/src/Pages/PixabayTest/PixabaySearch.js
+++ client/src/Pages/PixabayTest/PixabaySearch.js
@@ -9,52 +9,46 @@ import PixabayView from './PixabayView';
 class PixabaySearch extends Component {
     state = {
         searchText: '',
-        amount: 15,
+        amount: 30,
         apiUrl: 'https://pixabay.com/api',
         apiKey: '33449732-b16fa09defbfef09ff64fd27e',
         images: []
     }
 
     onTextChange = e => {
-        this.setState({[e.target.name]: e.target.value}, () => {
-            axios
-        .get(`${this.state.apiUrl}/?key=${this.state.apiKey}&q=${this.state.searchText}&image_type=photo&per_page=${this.state.amount}&safesearch=true`)
-        .then(res => this.setState({images: res.data.hits}))
-        .catch(err => console.log(err))
-    });
+        const val = e.target.value;
+        this.setState({ [e.target.name]: e.target.value }, () => {
+            if (val === '') {
+                this.setState({ images: [] });
+            } else {
+                axios
+                    .get(`${this.state.apiUrl}/?key=${this.state.apiKey}&q=${this.state.searchText}&image_type=photo&per_page=${this.state.amount}&safesearch=true`)
+                    .then(res => this.setState({ images: res.data.hits }))
+                    .catch(err => console.log(err))
+            }
+        });
     }
-    
-    onAmountChange = (e, index, value) => this.setState({amount: value});
+
+    onAmountChange = (e, index, value) => this.setState({ amount: value });
 
     render() {
         console.log(this.state.images);
         return (
             <div>
-                <Box sx={{maxWidth: '100%',}}>
-                    <TextField 
+                <Box sx={{ maxWidth: '100%', }}>
+                    <TextField
                         fullWidth
                         name='searchText'
-                        id='standard-basic' 
-                        label="Bilder Suche" 
-                        variant="standard" 
+                        id='standard-basic'
+                        label="Bilder Suche"
+                        variant="standard"
                         value={this.state.searchText}
                         onChange={this.onTextChange}
                     />
-                    <br/>
-                    <Select
-                        name='amount'
-                        label='Anzahl Bilder'
-                        value={this.state.amount}
-                        onChange={this.onAmountChange} 
-                    >
-                        <MenuItem value={15}>15</MenuItem>
-                        <MenuItem value={30}>30</MenuItem>
-                        <MenuItem value={50}>50</MenuItem>
-                    </Select>
-                    <br/>
-                    {this.state.images.length > 0 ? 
-                    (<PixabayView images={this.state.images}/>) 
-                    : null}
+                    <br />
+                    {this.state.images.length > 0 ?
+                        (<PixabayView images={this.state.images} />)
+                        : null}
                 </Box>
             </div>
         )
