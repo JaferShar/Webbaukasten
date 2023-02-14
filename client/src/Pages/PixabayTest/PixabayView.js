f5a949832d7bee66b2e42b376d4b492b5bb11c44
JaferShar <jafer.shar@gmail.com>
2023-02-14 14:37:53 +0100

    Button zum hochladen von Bildern in die Datenbank hinzugefügt

--- client/src/Pages/PixabayTest/PixabayView.js
+++ client/src/Pages/PixabayTest/PixabayView.js
@@ -10,56 +10,59 @@ import ImageListItem from '@mui/material/ImageListItem';
 import ImageListItemBar from '@mui/material/ImageListItemBar';
 import InfoIcon from '@mui/icons-material/Info';
 import ListSubheader from '@mui/material/ListSubheader';
+import { Button } from "@mui/material";
+import DownloadIcon from '@mui/icons-material/Download';
+import { createTheme } from '@mui/material/styles';
+import { PixabaySaveButton } from "./PixabaySaveButton";
+
 
 class PixabayView extends Component {
-    render() {
-        let imageListContent;
-        const { images } = this.props;
 
-        if (images) {
-            imageListContent = (
-                <ImageList sx={{ width: 1400, height: 900 }} cols={2}>
-                <ImageListItem key="Subheader" cols={2}>
-                  <ListSubheader component="div">Bilder</ListSubheader>
-                </ImageListItem>
-                {images.map((item) => (
-                  <ImageListItem key={item.id}>
-                    <img
-                      src={`${item.largeImageURL}?w=248&fit=crop&auto=format`}
-                      srcSet={`${item.largeImageURL}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                      alt={item.tags}
-                      loading="lazy"
-                    />
-                    <ImageListItemBar
-                      title={item.tags}
-                      subtitle={item.author}
-                      actionIcon={
-                        <IconButton
-                          sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
-                          aria-label={`info about ${item.tags}`}
-                        >
-                          <InfoIcon />
-                        </IconButton>
-                      }
-                    />
-                  </ImageListItem>
-                ))}
-              </ImageList>
-            )
-        } else {
-            imageListContent = null;
-        }
-        return (
-            <div>
-                {imageListContent}
-                
-            </div>
-        );
+ 
+
+  render() {
+    let imageListContent;
+    const { images } = this.props;
+
+    if (images) {
+      imageListContent = (
+        <ImageList sx={{ width: 1400, height: 900 }} cols={2}>
+          <ImageListItem key="Subheader" cols={2}>
+            <ListSubheader component="div">Bilder</ListSubheader>
+          </ImageListItem>
+          {images.map((item) => (
+            <ImageListItem key={item.id}>
+              <img
+                src={`${item.largeImageURL}?w=248&fit=crop&auto=format`}
+                srcSet={`${item.largeImageURL}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                alt={item.tags}
+                loading="lazy"
+              />
+              <ImageListItemBar
+                title={item.tags}
+                subtitle={item.author}
+                actionIcon={
+                  <PixabaySaveButton/>
+                }
+              />
+            </ImageListItem>
+          ))}
+        </ImageList>
+      )
+    } else {
+      imageListContent = null;
     }
+    return (
+      <div>
+        {imageListContent}
+
+      </div>
+    );
+  }
 }
 
 PixabayView.propTypes = {
-    images: PropTypes.array.isRequired
+  images: PropTypes.array.isRequired
 }
 
 export default PixabayView;
