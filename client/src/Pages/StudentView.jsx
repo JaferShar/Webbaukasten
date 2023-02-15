38397d76731bacbc842bf67e01f3cc70b9930f1b
Raphael Sutter <raphael39@users.noreply.github.com>
2023-02-15 22:03:03 +0100

    created DummyCourseData and Demo for student View

--- client/src/Pages/StudentView.jsx
+++ client/src/Pages/StudentView.jsx
@@ -1,9 +1,33 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Stack from '@mui/material/Stack';
+import { styled } from '@mui/material/styles';
+import data from '../assets/DummyCourse.json';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+
 function StudentView() {
-    return ( 
+    return (
         <div>
-            <h1>Student View</h1>
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <Box sx={{ width: '70%', margin: 'auto' }}>
+                    <Stack spacing={2}>
+                        <h1>{data.courseName}</h1>
+                        <Item>{data.screens[0].elements[0]}</Item>
+                        <img src={data.screens[0].elements[1]} alt="description of the image" />
+                    </Stack>
+                </Box>
+            </div>
         </div>
-     );
+    );
 }
 
 export default StudentView;
