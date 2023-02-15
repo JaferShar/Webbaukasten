b6c8ec02e51526aa8e4b471804c105f19fc4b975
JaferShar <jafer.shar@gmail.com>
2023-02-15 04:02:23 +0100

    html funktioniert!

---
+++ client/src/Pages/H5PTestFolder/H5PPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+export default function H5PPage() {
+
+    return (
+      <div className="gatsby-resp-iframe-wrapper">
+        <iframe src="https://h5p.org/h5p/embed/1352034" width="1090" height="335" frameborder="0" allowfullscreen="allowfullscreen" allow="geolocation *; microphone *; camera *; midi *; encrypted-media *" title="Bananen Test"></iframe><script src="https://h5p.org/sites/all/modules/h5p/library/js/h5p-resizer.js" charset="UTF-8"></script>
+      </div>
+    )
+  
+}
