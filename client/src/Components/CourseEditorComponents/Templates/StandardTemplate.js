adbe41d6195cfd7be49a960e62abd4fcc539f146
LauraKer <laura@kernahan.de>
2023-02-16 18:24:41 +0100

    Standard Template initialize

---
+++ client/src/Components/CourseEditorComponents/Templates/StandardTemplate.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "../../../Styling/ButtonStyling/addButton.css";
+import NextButton from "../Buttons/NextButton.js";
+import "../../../Styling/ButtonStyling/nextButton.css";
+import "../../../Styling/SiteStyling/BeginTemplate.css";
+import MenuBeginTemplate from "../Menus/ScreenMenu";
+
+const StandardTemplate = () => (
+  <div className="beginTemplate">
+    <div className="nextButton">
+      <NextButton />
+    </div>
+    <div>
+      <MenuBeginTemplate />
+    </div>
+  </div>
+);
+
+export default StandardTemplate;
