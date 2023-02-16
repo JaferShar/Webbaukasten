f86d52dfc4f374ef927623d7a864e22edb1db6f9
Raphael Sutter <raphael39@users.noreply.github.com>
2023-02-16 21:26:18 +0100

    added express server connection for student and some cleanup

--- server/server.js
+++ server/server.js
@@ -18,26 +18,10 @@ app.use("/api/account", require("./routes/accountRoutes"));
 app.use("/api/screen", require("./routes/screenRoutes"));
 app.use("/api/section", require("./routes/sectionRoutes"));
 app.use("api/element", require("./routes/elementRoutes"));
+app.use("api/student", require("./routes/studentRoutes"));
 
-//app.use('/api/users', require('./routes/userRoutes'));
-
-/*
-// Serve frontend
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
-
-  app.get('*', (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
-    )
-  );
-} else {
-  app.get('/', (req, res) => res.send('Please set to production'));
-}
-*/
 app.use(errorHandler);
 
-//app.listen(port, () => console.log(`Server started on port ${port}`));
 app.listen(port, () =>
   console.log(`Ahoi Matrosen wir segeln zum Port Hamburg`.yellow.bold)
 );
