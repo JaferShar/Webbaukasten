256d21ca43c246bee38a08351ac890ae0e409c29
Nick Oelmann <nick.oelmann@web.de>
2023-02-16 21:38:33 +0100

    created templates structure

---
+++ client/src/Components/CourseEditorComponents/Templates/Template.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ElementList from "../Elements/ElementList";
+
+const templates = {
+  Welcome: <ElementList />,
+  Standard: <ElementList />,
+  End: <ElementList />,
+};
+
+export default templates;
