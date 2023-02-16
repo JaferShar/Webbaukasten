5d3c4b10022b0e8275a4845e7126f66598e172c2
Nick Oelmann <nick.oelmann@web.de>
2023-02-16 19:11:52 +0100

    added dynamic h5p items

--- client/src/Pages/ScreenTest.jsx
+++ client/src/Pages/ScreenTest.jsx
@@ -72,7 +72,7 @@ function ElementList() {
       type: "pic",
       data: "https://picsum.photos/200/300",
     },
-    { id: 3, type: "h5p", data: "https://h5p.org" },
+    { id: 3, type: "h5p", data: "https://h5p.org/h5p/embed/1352034", title: "Bananen Test", height: "334" },
   ]);
 
   useEffect(() => {
@@ -113,9 +113,7 @@ function Element({ element }) {
   } else if (element.type === "h5p") {
     return (
       <ListItem>
-        <div className="gatsby-resp-iframe-wrapper">
-         <iframe src="https://h5p.org/h5p/embed/1352034" width="1090" height="335" frameborder="0" allowfullscreen="allowfullscreen" allow="geolocation *; microphone *; camera *; midi *; encrypted-media *" title="Bananen Test"></iframe><script src="https://h5p.org/sites/all/modules/h5p/library/js/h5p-resizer.js" charset="UTF-8"></script>
-       </div>
+         <iframe src={element.data} width="100%" frameborder="0" title={element.title}/>
       </ListItem>
     );
   }
