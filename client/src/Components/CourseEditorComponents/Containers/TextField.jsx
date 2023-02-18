efa42b803c241258dc39d203a91dd49d52a61a8f
Nick Oelmann <nick.oelmann@web.de>
2023-02-18 20:17:46 +0100

    Changed Add Button for adding a textfield to screen

--- client/src/Components/CourseEditorComponents/Containers/TextField.jsx
+++ client/src/Components/CourseEditorComponents/Containers/TextField.jsx
@@ -3,17 +3,22 @@ import { Button, Box } from "@mui/material";
 import { useSelector, useDispatch } from "react-redux";
 import { setTextField } from "../../../features/courseEditor/screenSlice";
 
-
 const TextField = () => {
-    const screen = useSelector((state) => state.screenEditor.screen);
-    const dispatch = useDispatch();
-    const handleCreateTextField = () => {
-        dispatch(setTextField({ screenId: screen._id, text: ""}));
-    };
+  const screen = useSelector((state) => state.screenEditor.screen);
+  const dispatch = useDispatch();
+  const handleCreateTextField = () => {
+    dispatch(setTextField({ screenId: screen._id, text: "" }));
+  };
   return (
     <Box>
-      <Button onClick={handleCreateTextField}>
+      <Button
+        onClick={handleCreateTextField}
+        style={{
+          border: "1px solid #d9dddd",
+        }}
+      >
         <AddIcon />
+        Textfeld erstellen
       </Button>
     </Box>
   );
