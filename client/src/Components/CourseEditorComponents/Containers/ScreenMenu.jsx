f4256634698ef692459279942dd1d53070a00a72
Nick Oelmann <nick.oelmann@web.de>
2023-02-19 18:42:39 +0100

    fixed react dom bug

--- client/src/Components/CourseEditorComponents/Containers/ScreenMenu.jsx
+++ client/src/Components/CourseEditorComponents/Containers/ScreenMenu.jsx
@@ -24,7 +24,7 @@ function TabPanel(props) {
     >
       {value === index && (
         <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
+          {children}
         </Box>
       )}
     </div>
