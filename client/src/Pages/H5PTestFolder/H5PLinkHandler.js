b2d8b90e88ec36504190c815f520c0db0008d709
JaferShar <jafer.shar@gmail.com>
2023-02-19 18:13:11 +0100

    Styling angepasst von Hochlade Button

--- client/src/Pages/H5PTestFolder/H5PLinkHandler.js
+++ client/src/Pages/H5PTestFolder/H5PLinkHandler.js
@@ -1,27 +1,27 @@
-import React, { Component } from 'react';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
+import React, { Component } from "react";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import H5PRedirection from "./H5PRedirection";
+import Grid from "@mui/material/Grid";
 
-export default function H5PLinkHandler(props){
-
-        return (
-            <div>
-                <TextField
-                    fullWidth
-                    name='h5p-element'
-                    id='standard-basic'
-                    label="H5P-Link"
-                    variant="standard"
-                    value={props.currString}
-                    onChange={props.updateString}
-                    InputProps={{
-                        endAdornment:
-                            <Button variant="contained" onClick={props.generateH5P}>
-                                Hochladen
-                            </Button>
-                    }}
-                />
-            </div>
-        )
-
+export default function H5PLinkHandler(props) {
+  return (
+    <div>
+      <Grid container columnGap={3}>
+        <TextField
+          name="h5p-element"
+          id="standard-basic"
+          label="H5P-Link"
+          variant="standard"
+          value={props.currString}
+          onChange={props.updateString}
+          sx={{ width: "75%" }}
+        />
+        <Button variant="contained" onClick={props.generateH5P}>
+          Hochladen
+        </Button>
+        <H5PRedirection />
+      </Grid>
+    </div>
+  );
 }
