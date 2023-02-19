524f69829c6058e71b1d450c867f6bc62968aad9
JaferShar <jafer.shar@gmail.com>
2023-02-19 16:57:58 +0100

    Darstellung des Pixabay Modals optimiert

--- client/src/Pages/PixabayTest/PixabayTestPage.js
+++ client/src/Pages/PixabayTest/PixabayTestPage.js
@@ -10,7 +10,7 @@ const style = {
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: 1400,
+    width: '75%',
     bgcolor: 'background.paper',
     border: '2px solid #000',
     boxShadow: 24,
