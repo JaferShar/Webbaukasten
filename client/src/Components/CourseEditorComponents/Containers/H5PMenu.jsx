436f8b427bff262eb5b53916ff763461f51e040f
Nick Oelmann <nick.oelmann@web.de>
2023-02-19 20:32:16 +0100

    added linking from Jafer

--- client/src/Components/CourseEditorComponents/Containers/H5PMenu.jsx
+++ client/src/Components/CourseEditorComponents/Containers/H5PMenu.jsx
@@ -1,16 +1,19 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { setH5P } from "../../../features/courseEditor/screenSlice";
-import AddIcon from "@mui/icons-material/Add";
 import { Button, Box, TextField } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import H5PLogo from "../../../assets/H5PLogo.jpg";
 
 const H5P = () => {
-    const screen = useSelector((state) => state.screenEditor.screen);
-    const dispatch = useDispatch();
+  const screen = useSelector((state) => state.screenEditor.screen);
+  const dispatch = useDispatch();
+  const h5pWebsite = "https://h5p.org/content-types-and-applications";
   const [value, setValue] = useState("");
   const handleClick = () => {
-    dispatch(setH5P({ screenId: screen._id, content: value }))
-    setValue("")
+    dispatch(setH5P({ screenId: screen._id, content: value }));
+    setValue("");
   };
   return (
     <Box>
@@ -21,18 +24,37 @@ const H5P = () => {
         variant='standard'
         value={value}
         onChange={(e) => setValue(e.target.value)}
-        sx={{ width: "75%" }}
+        sx={{ width: "50%" }}
       />
       <Button
         onClick={handleClick}
         style={{
-          border: "1px solid #d9dddd",         
+          border: "1px solid #d9dddd",
         }}
-        sx={{ pt: '10px', pb: '10px', pl: '20px', pr: '20px', ml: '1', mt: '1', mb: '1'}}
+        sx={{
+          pt: "10px",
+          pb: "10px",
+          pl: "20px",
+          pr: "20px",
+        }}
       >
         <AddIcon />
         Hochladen
       </Button>
+
+        <Button
+          component={Link}
+          to={h5pWebsite}
+          target='_blank'
+          variant='text'
+          size='small'
+        >
+          <img
+          src={H5PLogo}
+          alt="H5P Button"
+          style={{ width: 100, height: 45 }}
+        />
+        </Button>
     </Box>
   );
 };
