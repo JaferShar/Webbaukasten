33f1d94f4201e2c82392bd28e39cd3da1c36cd77
Raphael Sutter <raphael39@users.noreply.github.com>
2023-02-20 20:18:20 +0100

    added Handler for StudentView Template Selector

---
+++ client/src/Components/CourseEditorComponents/Templates/StudentTemplate.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ElementList from "../Elements/ElementList";
+
+const templates = {
+  Welcome: (<div>Hello</div>),
+  Standard: <ElementList />,
+  End: <div>End</div>
+};
+
+export default templates;
