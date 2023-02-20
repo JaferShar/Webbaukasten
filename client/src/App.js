7dd00cc20a4eaab383e3de0e2fdaee216cd6ccb1
Nick Oelmann <nick.oelmann@web.de>
2023-02-20 20:39:02 +0100

    deleted old dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -5,9 +5,6 @@ import Course from './Pages/CourseEditor'
 import Login from './Pages/Login'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Test from './Components/CourseEditorComponents/FixComponents/Test';
-import H5PPage from './Pages/H5PTestFolder/H5PPage'
-import PixabayTestPage from './Pages/PixabayTest/PixabayTestPage';
 import StudentView from './Pages/StudentView';
 import Profile from './Pages/ProfileSettings';
 
@@ -20,9 +17,6 @@ function App() {
           <Route path='/kurs' element={<Course />} />
           <Route path='/login' element={<Login />} />
           <Route path='/student/view' element={<StudentView />} />
-          <Route path='/pixabay' element={<PixabayTestPage />}/>
-          <Route path='/h5p' element={<H5PPage/>}/>
-          <Route path='/test' element={<Test />} />
           <Route path='/profile' element={<Profile />} />
         </Routes>
       </BrowserRouter>
