1c63a316b41df8b74cfb9ee097210ab148e9bc5f
Raphael Sutter <raphael39@users.noreply.github.com>
2023-02-21 23:20:20 +0100

    implemented List for StudentView viewing Standard Template

--- client/src/Components/CourseEditorComponents/Elements/StudentsElementList.jsx
+++ client/src/Components/CourseEditorComponents/Elements/StudentsElementList.jsx
@@ -1,9 +1,33 @@
-import { useEffect } from "react";
 import { Stack } from "@mui/material";
 import Element from "./Element";
 import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
 
 export default function StudentElementList() {
- 
-    return <div>Hello</div>
+    const studentScreen = useSelector(
+        (state) => state.studentScreen
+    );
+
+  const dispatch = useDispatch();
+
+  useEffect (() => {
+    }, [studentScreen.elements])
+
+  if (!studentScreen.screen.elements || studentScreen.screen.elements.length === 0) {
+    return <Stack spacing={2} >
+        <div>Failed to load elements of screen</div>
+        </Stack>;
+  } else {
+    return (
+      <Stack spacing={2}>
+        {studentScreen.screen.elements.map((element) => (
+          <Element
+          key={element._id}
+          element={element}
+          style={{ cursor: "context-menu" }}
+        />
+        ))}
+      </Stack>
+    );
+  }
 }
