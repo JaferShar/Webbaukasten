c710697e386485f2091c00b8170ffe1b6c3c2d76
Raphael Sutter <raphael39@users.noreply.github.com>
2023-02-21 22:24:59 +0100

    init for studentView elementlist for standard view

---
+++ client/src/Components/CourseEditorComponents/Elements/StudentsElementList.jsx
@@ -0,0 +1,9 @@
+import { useEffect } from "react";
+import { Stack } from "@mui/material";
+import Element from "./Element";
+import { useDispatch, useSelector } from "react-redux";
+
+export default function StudentElementList() {
+ 
+    return <div>Hello</div>
+}
