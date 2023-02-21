86741273274bb7968039ac9359b30deda5057f7c
Nick Oelmann <nick.oelmann@web.de>
2023-02-21 19:22:18 +0100

    added context menu for element options

--- client/src/Components/CourseEditorComponents/Elements/ElementList.jsx
+++ client/src/Components/CourseEditorComponents/Elements/ElementList.jsx
@@ -5,13 +5,12 @@ import { useDispatch, useSelector } from "react-redux";
 import { getScreen } from "../../../features/courseEditor/screenSlice";
 
 export default function ElementList() {
-  const dispatch = useDispatch();
   const elements = useSelector((state) => state.screenEditor.screen.elements);
 
-  // maybe dispatch later to be current
-  useEffect(() => {
-      //dispatch(getScreen());
-  }, [elements]);
+  const handleContextMenu = (event, elementId) => {
+    console.log("context menu", elementId);
+    event.preventDefault();
+  };
 
   if (!elements || elements.length === 0) {
     return <Stack spacing={2} />;
@@ -19,7 +18,12 @@ export default function ElementList() {
     return (
       <Stack spacing={2}>
         {elements.map((element) => (
-          <Element key={element._id} element={element} />
+          <Element
+            key={element._id}
+            element={element}
+            handleContextMenu={handleContextMenu}
+            style={{ cursor: "context-menu" }}
+          />
         ))}
       </Stack>
     );
