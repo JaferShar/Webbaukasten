b9c9e7264b2bb5f455929ba32de293f84b7f9350
Nick Oelmann <nick.oelmann@web.de>
2023-02-21 20:18:18 +0100

    implemented delete in menu

--- client/src/Components/CourseEditorComponents/Elements/ElementMenu.jsx
+++ client/src/Components/CourseEditorComponents/Elements/ElementMenu.jsx
@@ -1,18 +1,32 @@
 import React from "react";
-import { Box, Menu, MenuItem, Accordion, AccordionSummary, AccordionDetails, List, ListItem, ListItemText } from "@mui/material";
+import {
+  Box,
+  Menu,
+  MenuItem,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  List,
+  ListItemButton,
+  ListItemText,
+} from "@mui/material";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 export default function ElementMenu({ anchorEl, handleClose, handleDelete }) {
   return (
     <Box sx={{ maxHeight: "80vh", overflow: "auto" }}>
       <Menu
-        id="long-menu"
+        id='long-menu'
         anchorEl={anchorEl}
         keepMounted
         open={Boolean(anchorEl)}
         onClose={handleClose}
       >
-        <MenuItem onClick={handleDelete}>Löschen</MenuItem>
+        <MenuItem onClick={handleDelete}>
+          <DeleteIcon sx={{ mr: 1 }} />
+          Löschen
+        </MenuItem>
         <SubMenu />
       </Menu>
     </Box>
@@ -22,18 +36,20 @@ export default function ElementMenu({ anchorEl, handleClose, handleDelete }) {
 function SubMenu() {
   return (
     <Accordion>
-      <AccordionSummary expandIcon={<ExpandMoreIcon />}>Austauschen</AccordionSummary>
+      <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+        Austauschen
+      </AccordionSummary>
       <AccordionDetails sx={{ padding: "8px 16px" }}>
         <List>
-          <ListItem button sx={{ paddingTop: "8px", paddingBottom: "8px" }}>
-            <ListItemText primary="Textfeld" />
-          </ListItem>
-          <ListItem button sx={{ paddingTop: "8px", paddingBottom: "8px" }}>
-            <ListItemText primary="Bild" />
-          </ListItem>
-          <ListItem button sx={{ paddingTop: "8px", paddingBottom: "8px" }}>
-            <ListItemText primary="H5P" />
-          </ListItem>
+          <ListItemButton sx={{ paddingTop: "8px", paddingBottom: "8px" }}>
+            <ListItemText primary='Textfeld' />
+          </ListItemButton>
+          <ListItemButton sx={{ paddingTop: "8px", paddingBottom: "8px" }}>
+            <ListItemText primary='Bild' />
+          </ListItemButton>
+          <ListItemButton sx={{ paddingTop: "8px", paddingBottom: "8px" }}>
+            <ListItemText primary='H5P' />
+          </ListItemButton>
         </List>
       </AccordionDetails>
     </Accordion>
