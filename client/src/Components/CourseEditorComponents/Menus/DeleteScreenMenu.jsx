25bc95ab4483ecd45dd10d021407d09820f6950f
Nick Oelmann <nick.oelmann@web.de>
2023-02-21 20:17:39 +0100

    changed name convention

--- client/src/Components/CourseEditorComponents/Menus/DeleteScreenMenu.jsx
+++ client/src/Components/CourseEditorComponents/Menus/DeleteScreenMenu.jsx
@@ -3,18 +3,22 @@ import { Menu, MenuItem } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
 import Container from "@mui/material/Container";
 
-export default function DeleteScreenMenu({ anchorEl, handleDelete, handleClose }) {
+export default function DeleteScreenMenu({
+  anchorEl,
+  handleDelete,
+  handleClose,
+}) {
   return (
     <Container>
       <Menu
-        id="long-menu"
+        id='long-menu'
         anchorEl={anchorEl}
         keepMounted
         open={Boolean(anchorEl)}
         onClose={handleClose}
       >
         <MenuItem onClick={handleDelete}>
-          <DeleteIcon /> Delete
+          <DeleteIcon /> Löschen
         </MenuItem>
       </Menu>
     </Container>
