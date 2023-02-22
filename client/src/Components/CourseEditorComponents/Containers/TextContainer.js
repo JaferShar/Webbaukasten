d83a18b81d00c8afa55896d3190ef98057568fe1
Jasmin Parmar <uczyf@student.kit.edu>
2023-02-22 19:38:36 +0100

    changed editor and added it to menu button

--- client/src/Components/CourseEditorComponents/Containers/TextContainer.js
+++ client/src/Components/CourseEditorComponents/Containers/TextContainer.js
@@ -1,38 +1,58 @@
-import React, { useState } from "react";
-import '../../../Styling/SiteStyling/TextContainer.css';
-import { EditorState, Editor } from 'draft-js';
+import React, { useCallback, useState } from "react";
+import RichTextEditor from "react-rte-17";
 
-const TextContainer = ({ text }) => <div>{text}</div>;
+
 
 export default function PopUpButtonText() {
-  const [showEditor, setShowEditor] = useState(false);
-  const [editorState, setEditorState] = useState(() =>
-    EditorState.createEmpty()
-  );
-  const [textContent, setTextContent] = useState('');
 
-  const handleSave = () => {
-    setTextContent(
-      editorState.getCurrentContent().getPlainText()
-    );
+
+const rteText = ``;
+const [html, set] = useState("");
+  const toolbarConfig = {
+    
+    display: [
+      "INLINE_STYLE_BUTTONS",
+      "BLOCK_TYPE_BUTTONS",
+      "BLOCK_TYPE_DROPDOWN",
+      "HISTORY_BUTTONS"
+    ],
+    INLINE_STYLE_BUTTONS: [
+      { label: "Bold", style: "BOLD" },
+      { label: "Italic", style: "ITALIC" },
+      { label: "Underline", style: "UNDERLINE" }
+    ],
+    BLOCK_TYPE_DROPDOWN: [
+      { label: "Normal", style: "unstyled" },
+      { label: "Heading Large", style: "header-one" },
+      { label: "Heading Medium", style: "header-two" },
+      { label: "Heading Small", style: "header-three" }
+    ],
+    BLOCK_TYPE_BUTTONS: [
+      { label: "UL", style: "unordered-list-item" },
+      { label: "OL", style: "ordered-list-item" }
+    ]
   };
 
+  const [notes, setNotes] = useState(
+    RichTextEditor.createValueFromString(rteText, "html")
+  );
+
+
+  const onValueChangeCallback = useCallback((value) => {
+    const toHtml = value.toString("html");
+    setNotes(value);
+    console.log(toHtml);
+    set(toHtml);
+    // onValueChange(value.toString('html'));
+  }, []);
+
   return (
-    <div>
-      <button onClick={() => setShowEditor(!showEditor)}>
-        Editor
-      </button>
-      {showEditor && (
-        <div style={{ border: "1px solid black", padding: '2px', minHeight: '200px'}}>
-          <Editor
-            editorState={editorState}
-            onEditorStateChange={setEditorState}
-          />
-          <button onClick={handleSave}>Save</button>
-        </div>
-      )}
-      <TextContainer text={textContent} />
-    </div>
+    <RichTextEditor
+    value={notes}
+    onChange={onValueChangeCallback}
+    data-test="notes"
+    toolbarConfig={toolbarConfig}
+  />
   );
 }
 
--- client/src/Components/CourseEditorComponents/Containers/TextFieldMenu.jsx
+++ client/src/Components/CourseEditorComponents/Containers/TextFieldMenu.jsx
@@ -1,27 +1,57 @@
+import React, { useState } from "react";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@mui/material";
+import PopUpButtonText from "./TextContainer";
 import AddIcon from "@mui/icons-material/Add";
-import { Button, Box } from "@mui/material";
-import { useSelector, useDispatch } from "react-redux";
-import { setTextField } from "../../../features/courseEditor/screenSlice";
 
-const TextField = () => {
-  const screen = useSelector((state) => state.screenEditor.screen);
-  const dispatch = useDispatch();
-  const handleCreateTextField = () => {
-    dispatch(setTextField({ screenId: screen._id, text: "" }));
+const TextEditor = () => {
+  const [open, setOpen] = useState(false);
+  const [text] = useState("");
+
+  const handleOpen = () => {
+    setOpen(true);
   };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleSave = () => {
+    console.log(`Text: ${text}`);
+    handleClose();
+  };
+
   return (
-    <Box>
+    <div>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Text Editor</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Edit the text below and click Save to submit changes.
+          </DialogContentText>
+          <PopUpButtonText />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleSave}>Save</Button>
+        </DialogActions>
+      </Dialog>
       <Button
-        onClick={handleCreateTextField}
-        style={{
-          border: "1px solid #d9dddd",
-        }}
+        style={{ border: "1px solid #d9dddd" }}
+        variant="contained"
+        onClick={handleOpen}
       >
         <AddIcon />
-        Textfeld erstellen
+        Text hinzufügen
       </Button>
-    </Box>
+    </div>
   );
 };
 
-export default TextField;
+export default TextEditor;
