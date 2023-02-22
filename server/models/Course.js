ccccd94242baa8f41644399df5b4fcc609b94b49
Nick Oelmann <nick.oelmann@web.de>
2023-02-22 21:53:38 +0100

    commented Course

--- server/models/Course.js
+++ server/models/Course.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose");
 
+/**
+ * This mongoose schema defines the structure of a course document in the database.
+ */
 const courseSchema = new mongoose.Schema({
+  // The account that owns this course is referenced as ObjectId.
   account: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Account",
@@ -22,24 +26,34 @@ const courseSchema = new mongoose.Schema({
         },
       },
     ],
+    // set default value to the sections array with one section
     default: function () {
       return [{ sectionName: this.courseName, startIndex: 0 }];
     },
   },
+  // The screens that belong to this course are referenced as ObjectId.
   screens: {
     type: [mongoose.Schema.Types.ObjectId],
     ref: "Screen",
   },
 });
 
+/**
+ * This method changes the position of a screen in the course.
+ * @param {ObjectId} screenId the screen to move
+ * @param {int} newIndex the new index of the screen
+ */
 courseSchema.methods.updateScreenPosition = function (screenId, newIndex) {
   const screenIndex = this.screens.indexOf(screenId);
   if (screenIndex === -1) {
     throw new Error("Screen not found in course screens");
   }
-
+  // Remove the screen from the array and insert it at the new index.
   this.screens.splice(screenIndex, 1);
   this.screens.splice(newIndex, 0, screenId);
 };
 
+/**
+* Export the schema as a mongoose model.
+*/
 module.exports = mongoose.model("Course", courseSchema);
