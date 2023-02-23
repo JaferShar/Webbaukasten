4e061c26a53470533069e4594a1f3d44b345b54a
Nick Oelmann <nick.oelmann@web.de>
2023-02-23 17:56:33 +0100

    updated upload to return image url

--- client/src/features/upload/CloudinaryUpload.js
+++ client/src/features/upload/CloudinaryUpload.js
@@ -1,11 +1,7 @@
 import axios from "axios";
-import { useSelector, useDispatch } from "react-redux";
-import { setPicture } from "../courseEditor/screenSlice";
 import { toast } from "react-toastify";
 
 const CloudinaryUpload = async (file) => {
-  const dispatch = useDispatch();
-  const screen = useSelector((state) => state.screenEditor.screen);
 
   const cloudName = process.env.REACT_APP_CLOUD_NAME;
   const uploadPreset = process.env.REACT_APP_UPLOAD_PRESET;
@@ -20,7 +16,7 @@ const CloudinaryUpload = async (file) => {
       `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`,
       formData
     );
-    dispatch(setPicture({ url: result.data.secure_url, screenId: screen._id }));
+    return result.data.secure_url;
   } catch (error) {
     toast.error("Upload failed. Please try again later.");
   }
