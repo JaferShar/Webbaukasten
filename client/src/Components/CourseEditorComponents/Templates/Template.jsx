ca73ff4e55bc680bd1d171c8eb4750a3e8ece0ef
Jasmin Parmar <uczyf@student.kit.edu>
2023-02-24 00:15:54 +0100

    formatted and commented screen and templates

--- client/src/Components/CourseEditorComponents/Templates/Template.jsx
+++ client/src/Components/CourseEditorComponents/Templates/Template.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ElementList from "../Elements/ElementList";
 import WelcomeTemplateEditor from "./WelcomeTemplateEditor";
-
+/** 
+ * A component containing templates for creating screens.
+ * */
 const templates = {
-  Welcome: <WelcomeTemplateEditor/>,
+  Welcome: <WelcomeTemplateEditor />,
   Standard: <ElementList />,
 };
 
