e4e24d54a5daa8b2be0cbeda58739e860dd0681d
Jasmin Parmar <uczyf@student.kit.edu>
2023-02-23 23:55:51 +0100

    formatted and commented menus folder

--- client/src/Components/CourseEditorComponents/Menus/AddScreenMenu.jsx
+++ client/src/Components/CourseEditorComponents/Menus/AddScreenMenu.jsx
@@ -3,12 +3,17 @@ import { Menu, MenuItem } from "@mui/material";
 import TitleIcon from "@mui/icons-material/Title";
 import SourceIcon from "@mui/icons-material/Source";
 import Container from "@mui/material/Container";
-
-export default function AddScreenMenu({
-  anchorEl,
-  handleClose,
-  handleCreate
-}) {
+/**
+ * This module provides a menu that allows a standard or welcome template to be created.
+ *
+ * @param {*} {
+ *   anchorEl The anchor element to attach the menu to.
+ *   handleClose, The function to call when the menu is closed.
+ *   handleCreate, The function to call when a new screen is created
+ * }
+ * @returns menu tab with standard and welcome templates.
+ */
+export default function AddScreenMenu({ anchorEl, handleClose, handleCreate }) {
   const handleClick = (template) => {
     handleCreate(template);
     handleClose();
@@ -22,12 +27,18 @@ export default function AddScreenMenu({
         open={Boolean(anchorEl)}
         onClose={handleClose}
       >
-        <MenuItem onClick={() => {
-          handleClick("Welcome")}}>
+        <MenuItem
+          onClick={() => {
+            handleClick("Welcome");
+          }}
+        >
           <TitleIcon /> Wilkommen
         </MenuItem>
-        <MenuItem onClick={() => {
-          handleClick("Standard")}}>
+        <MenuItem
+          onClick={() => {
+            handleClick("Standard");
+          }}
+        >
           <SourceIcon />
           Standard
         </MenuItem>
