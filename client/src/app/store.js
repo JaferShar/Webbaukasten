0ca86bc0e570954aa1fd813e9d3101acaf5c7ec2
Jasmin Parmar <uczyf@student.kit.edu>
2023-02-23 19:44:34 +0100

    formated, commented H5P/pix menu and store

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,20 +1,21 @@
-import {configureStore } from '@reduxjs/toolkit';
-import authReducer from '../features/auth/authSlice';
-import courseOverViewReducer from '../features/courseOverview/courseOverViewSlice';
-import courseEditorReducer from '../features/courseEditor/courseSlice';
-import screenReudcer from '../features/courseEditor/screenSlice';
-import studentCourseReducer from '../features/studentView/studentCourseSlice';
-import studentScreenReducer from '../features/studentView/studentScreenSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "../features/auth/authSlice";
+import courseOverViewReducer from "../features/courseOverview/courseOverViewSlice";
+import courseEditorReducer from "../features/courseEditor/courseSlice";
+import screenReudcer from "../features/courseEditor/screenSlice";
+import studentCourseReducer from "../features/studentView/studentCourseSlice";
+import studentScreenReducer from "../features/studentView/studentScreenSlice";
 window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-
-
-export const store = configureStore({
-    reducer: {
-        auth: authReducer,
-        courseOverview: courseOverViewReducer,
-        courseEditor: courseEditorReducer,
-        screenEditor: screenReudcer,
-        studentCourse: studentCourseReducer,
-        studentScreen: studentScreenReducer,
-    },
+/** 
+ * Creates a Redux store with multiple reducers that handle the various component states.
+ */
+const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    courseOverview: courseOverViewReducer,
+    courseEditor: courseEditorReducer,
+    screenEditor: screenReudcer,
+    studentCourse: studentCourseReducer,
+    studentScreen: studentScreenReducer,
+  },
 });
