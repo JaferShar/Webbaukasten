6ea7c7060544909378849adc9625a8de68cd3525
Jasmin Parmar <uczyf@student.kit.edu>
2023-02-24 00:42:56 +0100

    formatted and commented search bars and headers

--- client/src/Components/Headers/ProfileHeader.jsx
+++ client/src/Components/Headers/ProfileHeader.jsx
@@ -1,24 +1,33 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import { logout, reset } from '../../features/auth/authSlice';
-import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import CloudDoneIcon from '@mui/icons-material/CloudDone';
-import HomeIcon from '@mui/icons-material/Home';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import { logout, reset } from "../../features/auth/authSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import CloudDoneIcon from "@mui/icons-material/CloudDone";
+import HomeIcon from "@mui/icons-material/Home";
 import { toast } from "react-toastify";
 
-
-const settings = ['Profile', 'Logout'];
-
+const settings = ["Profile", "Logout"];
+/**
+ * This module delivers a responsive app bar component for the coirse overview.
+ * It displays save and home buttons as well as user settings menu.
+ *
+ * @param {*} {
+ *   searchTerm, The current search term in the search bar.
+ *   onSearch, The function to call when the search term changes.
+ *   handleSearch, The function to call when the user submits a search.
+ * }
+ * @returns responsive app bar for the profile settings.
+ */
 function ResponsiveAppBar({ searchTerm, onSearch, handleSearch }) {
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -27,19 +36,19 @@ function ResponsiveAppBar({ searchTerm, onSearch, handleSearch }) {
 
   const handleOpenUserMenu = (event) => {
     setAnchorElUser(event.currentTarget);
-    console.log('handleOpenUserMenu');
+    console.log("handleOpenUserMenu");
     console.log(account);
   };
 
   const handleCloseUserMenu = async (clickedSetting) => {
-    if (clickedSetting === 'Logout') {
-      localStorage.removeItem('account');
+    if (clickedSetting === "Logout") {
+      localStorage.removeItem("account");
       await dispatch(reset());
       await dispatch(logout());
-      navigate('/login');
-    }else if (clickedSetting === 'Profile') {
-        navigate(`/profile?accountID=${account._id}`);
-      }
+      navigate("/login");
+    } else if (clickedSetting === "Profile") {
+      navigate(`/profile?accountID=${account._id}`);
+    }
 
     setAnchorElUser(null);
   };
@@ -49,36 +58,48 @@ function ResponsiveAppBar({ searchTerm, onSearch, handleSearch }) {
       <Container maxWidth="100%">
         <Toolbar disableGutters>
           <Box display="flex" flexGrow={1}>
-            {/* whatever is on the left side */}
-            <h1 style={{ color: 'white' }}>Profile</h1>
+            <h1 style={{ color: "white" }}>Profile</h1>
           </Box>
-          {/* this is on the right side*/}
-          <div style={{marginRight: "40px"}}>
-          <CloudDoneIcon onClick={() => {toast.success('Your changes have been saved.');}} style={{ cursor: 'pointer' }} />
+
+          <div style={{ marginRight: "40px" }}>
+            <CloudDoneIcon
+              onClick={() => {
+                toast.success("Your changes have been saved.");
+              }}
+              style={{ cursor: "pointer" }}
+            />
           </div>
-          <div style={{marginRight: "40px"}}>
-          <HomeIcon onClick={() => {navigate('/kursuebersicht');}} style={{ cursor: 'pointer' }} />
+          <div style={{ marginRight: "40px" }}>
+            <HomeIcon
+              onClick={() => {
+                navigate("/kursuebersicht");
+              }}
+              style={{ cursor: "pointer" }}
+            />
           </div>
 
-          {/* This is the profile Button and menu  */}
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src={account ? account.picture : ''} referrerPolicy="no-referrer"/>
+                <Avatar
+                  alt="Remy Sharp"
+                  src={account ? account.picture : ""}
+                  referrerPolicy="no-referrer"
+                />
               </IconButton>
             </Tooltip>
             <Menu
-              sx={{ mt: '45px' }}
+              sx={{ mt: "45px" }}
               id="menu-appbar"
               anchorEl={anchorElUser}
               anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
+                vertical: "top",
+                horizontal: "right",
               }}
               keepMounted
               transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
+                vertical: "top",
+                horizontal: "right",
               }}
               open={Boolean(anchorElUser)}
               onClose={handleCloseUserMenu}
--- client/src/Components/Headers/ResponsiveAppBar.jsx
+++ client/src/Components/Headers/ResponsiveAppBar.jsx
@@ -1,21 +1,32 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import { logout, reset } from '../../features/auth/authSlice';
-import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import SearchBar from '../CourseOverviewComponents/SearchBar';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import { logout, reset } from "../../features/auth/authSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import SearchBar from "../CourseOverviewComponents/SearchBar";
 
-const settings = ['Profile', 'Logout'];
+const settings = ["Profile", "Logout"];
 
+/**
+ * This module delivers a responsive app bar component for the coirse overview.
+ * It displays a logo, search bar, and user settings menu.
+ *
+ * @param {*} { 
+ *   searchTerm, The current search term in the search bar.
+ *   onSearch, The function to call when the search term changes.
+ *   handleSearch, The function to call when the user submits a search.
+ * }
+ * @returns responsive app bar for the course overview.
+ */
 function ResponsiveAppBar({ searchTerm, onSearch, handleSearch }) {
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -27,12 +38,12 @@ function ResponsiveAppBar({ searchTerm, onSearch, handleSearch }) {
   };
 
   const handleCloseUserMenu = async (clickedSetting) => {
-    if (clickedSetting === 'Logout') {
-      localStorage.removeItem('account');
+    if (clickedSetting === "Logout") {
+      localStorage.removeItem("account");
       await dispatch(reset());
       await dispatch(logout());
-      navigate('/login');
-    } else if (clickedSetting === 'Profile') {
+      navigate("/login");
+    } else if (clickedSetting === "Profile") {
       navigate(`/profile?accountID=${account._id}`);
     }
     setAnchorElUser(null);
@@ -43,32 +54,35 @@ function ResponsiveAppBar({ searchTerm, onSearch, handleSearch }) {
       <Container maxWidth="100%">
         <Toolbar disableGutters>
           <Box display="flex" flexGrow={1}>
-            {/* whatever is on the left side */}
-            <h1 style={{ color: 'white' }}>Kursübersicht</h1>
+            <h1 style={{ color: "white" }}>Kursübersicht</h1>
           </Box>
-          <div style={{marginRight: "20px"}}>
-            <SearchBar onSearch={handleSearch} searchTerm={searchTerm}/>
+          <div style={{ marginRight: "20px" }}>
+            <SearchBar onSearch={handleSearch} searchTerm={searchTerm} />
           </div>
 
           {/* This is the profile Button and menu */}
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src={account ? account.picture : ''} referrerPolicy="no-referrer" />
+                <Avatar
+                  alt="Remy Sharp"
+                  src={account ? account.picture : ""}
+                  referrerPolicy="no-referrer"
+                />
               </IconButton>
             </Tooltip>
             <Menu
-              sx={{ mt: '45px' }}
+              sx={{ mt: "45px" }}
               id="menu-appbar"
               anchorEl={anchorElUser}
               anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
+                vertical: "top",
+                horizontal: "right",
               }}
               keepMounted
               transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
+                vertical: "top",
+                horizontal: "right",
               }}
               open={Boolean(anchorElUser)}
               onClose={handleCloseUserMenu}
