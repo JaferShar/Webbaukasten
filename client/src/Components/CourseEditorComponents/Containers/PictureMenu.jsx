b708909e26baff96969b67ab4d4ca4a8f937c652
Nick Oelmann <nick.oelmann@web.de>
2023-02-23 22:51:31 +0100

    fixed and optimized widget from cloudinary

--- client/src/Components/CourseEditorComponents/Containers/PictureMenu.jsx
+++ client/src/Components/CourseEditorComponents/Containers/PictureMenu.jsx
@@ -1,29 +1,26 @@
-import React, { useEffect } from "react";
+import React, { useCallback } from "react";
 import AddIcon from "@mui/icons-material/Add";
 import { Button } from "@mui/material";
 import { useSelector, useDispatch } from "react-redux";
 import { setPicture } from "../../../features/courseEditor/screenSlice";
 import { toast } from "react-toastify";
+
 /**
- * This component provides a button to open Cloudinary upload widget.
- * Upon successful upload, the uploaded image is saved to the current screen in the Redux store.
- *
- * @export
- * @return {JSX.Element} CloudinaryUploadWidget Component
- */
+This component provides a button to open Cloudinary upload widget.
+Upon successful upload, the uploaded image is saved to the current screen in the Redux store.
+@export
+@return {JSX.Element} CloudinaryUploadWidget Component
+*/
 export default function CloudinaryUploadWidget() {
   const dispatch = useDispatch();
   const screen = useSelector((state) => state.screenEditor.screen);
-  const cloudName = process.env.REACT_APP_CLOUD_NAME;
-  const uploadPreset = process.env.REACT_APP_UPLOAD_PRESET;
-
-  // Hook to initialize and open Cloudinary upload widget.
-
-  useEffect(() => {
-    const widget = window.cloudinary.createUploadWidget(
+  const { REACT_APP_CLOUD_NAME, REACT_APP_UPLOAD_PRESET } = process.env;
+  // Memoize Cloudinary upload widget creation.
+  const widget = useCallback(() => {
+    window.cloudinary.createUploadWidget(
       {
-        cloudName: cloudName,
-        uploadPreset: uploadPreset,
+        cloudName: REACT_APP_CLOUD_NAME,
+        uploadPreset: REACT_APP_UPLOAD_PRESET,
         sources: [
           "local",
           "url",
@@ -33,28 +30,35 @@ export default function CloudinaryUploadWidget() {
           "dropbox",
         ],
       },
-      (error, result) => {
+      async (error, result) => {
         if (!error && result && result.event === "success") {
-          dispatch(
-            setPicture({ url: result.info.secure_url, screenId: screen._id })
-          );
+          try {
+            dispatch(setPicture({ url: result.info.secure_url, screenId: screen._id }));
+          } catch (error) {
+            toast.error("Failed to upload image");
+          }
         }
       }
     );
+  }, [REACT_APP_CLOUD_NAME, REACT_APP_UPLOAD_PRESET, dispatch, screen._id]);
 
-    document.getElementById("upload_widget").addEventListener(
-      "click",
-      () => {
-        widget.open();
-      },
-      false
-    );
-  }, [dispatch, screen._id, cloudName, uploadPreset]);
+
+  // Use ref hook to attach Cloudinary upload widget to button click event.
+  const buttonRef = useCallback(
+    (node) => {
+      if (node !== null) {
+        node.addEventListener("click", () => {
+          widget.open();
+        });
+      }
+    },
+    [widget]
+  );
 
   return (
     <Button
       style={{ border: "1px solid #d9dddd" }}
-      id='upload_widget'
+      ref={buttonRef}
       className='cloudinary-button'
     >
       <AddIcon />
