5e590740e164cbc1e5b5f625b05de3e0d11aaf8d
Nick Oelmann <nick.oelmann@web.de>
2023-02-24 12:51:04 +0100

    cleaned up Picture menu and made it adaptive

--- client/src/Components/CourseEditorComponents/Containers/PictureMenu.jsx
+++ client/src/Components/CourseEditorComponents/Containers/PictureMenu.jsx
@@ -1,73 +1,68 @@
-import React, { useEffect } from "react";
+import UploadWidget from "../../../features/upload/UploadWidget";
+import Button from "@mui/material/Button";
 import AddIcon from "@mui/icons-material/Add";
-import { Button } from "@mui/material";
 import { useSelector, useDispatch } from "react-redux";
 import { setPicture } from "../../../features/courseEditor/screenSlice";
+import { toast } from "react-toastify";
 
 /**
- * This method handles the logic for uploading and dispatching a picture to backend.
- * @param {*} dispatch 
- * @param {*} screen 
- * @returns 
+ * This component provides a button that opens a cloudinary upload widget which provides 
+ * some methods to upload pictures to the cloudinary database. 
+ * The uploaded picture is stored in the url field of the screen.
+ * @returns An upload widget for uploading pictures to the cloudinary database.
  */
-const handleUploadSuccess = (dispatch, screen) => (error, result) => {
-  if (!error && result && result.event === "success") {
-    dispatch(
-      setPicture({ url: result.info.secure_url, screenId: screen._id })
-    );
-  } else if (error) {
-    console.error(error);
-  }
-};
-
-/**
- * This Module is responsible for displaying the Cloudinary Upload Widget.
- * @returns The Cloudinary Upload Widget
- */
-export default function CloudinaryUploadWidget() {
+export default function PictureMenu() {
   const dispatch = useDispatch();
   const screen = useSelector((state) => state.screenEditor.screen);
-  const cloudName = process.env.REACT_APP_CLOUD_NAME;
-  const uploadPreset = process.env.REACT_APP_UPLOAD_PRESET;
 
-  const widget = window.cloudinary.createUploadWidget(
-    {
-      cloudName: cloudName,
-      uploadPreset: uploadPreset,
-      sources: [
-        "local",
-        "url",
-        "camera",
-        "image_search",
-        "google_drive",
-        "dropbox",
-      ],
-      googleApiKey: process.env.REACT_APP_GOOGLE_API_KEY,
-      searchByRights: true,
-    },
-    handleUploadSuccess(dispatch, screen)
-  );
-
-  useEffect(() => {
-    const handleClick = () => {
-      widget.open();
-    };
-
-    document.getElementById("upload_widget").addEventListener("click", handleClick);
-
-    return () => {
-      document.getElementById("upload_widget").removeEventListener("click", handleClick);
-    };
-  }, [widget]);
-
+  /**
+   * This function handles the upload of a picture to the cloudinary database.
+   * @param {*} error Any error that occured during the upload to cloudinary.
+   * @param {*} result Contains information about the uploaded picture.
+   * @param {*} widget The cloudinary widget that is used to upload the picture.
+   * @returns 
+   */
+  function handleOnUpload(error, result, widget) {
+    // If an error occured during the upload, the widget is closed and an error message is displayed.
+    if ( error ) {
+      toast.error(error.statusText);
+      widget.close({
+        quiet: true
+      });
+      return;
+    } else if ( result.event === "success" ) {
+      // If the upload was successful, the picture url is stored in the url field of the screen.
+      try {
+        dispatch(setPicture({ url: result.info.secure_url, screenId: screen._id }));
+      } catch (error) {
+        // If the picture url could not be stored in the url field of the screen, 
+        // the widget is closed and an error message is displayed.
+        toast.error("Bild konnte nicht hochgeladen werden.");
+        widget.close({
+          quiet: true
+        });
+        return;
+      }
+    }
+  }
+  // The upload widget is returned.
   return (
-    <Button
-      style={{ border: "1px solid #d9dddd" }}
-      id="upload_widget"
-      className="cloudinary-button"
-    >
-      <AddIcon />
-      Bild hochladen
-    </Button>
-  );
+    <div>
+        <UploadWidget onUpload={handleOnUpload}>
+          {({ open }) => {
+            function handleOnClick(e) {
+              e.preventDefault();
+              open();
+            }
+            return (
+              <Button onClick={handleOnClick}
+              style={{ border: "1px solid #d9dddd" }}>
+                <AddIcon />
+                Bild hochladen
+              </Button>
+            )
+          }}
+        </UploadWidget>
+      </div>
+  )
 }
