c4e7b6ee7af365adbc821cd87e8ce192bf08101a
Nick Oelmann <nick.oelmann@web.de>
2023-02-24 01:45:29 +0100

    finally implemented widget with bug fixes

--- client/src/Components/CourseEditorComponents/Containers/PictureMenu.jsx
+++ client/src/Components/CourseEditorComponents/Containers/PictureMenu.jsx
@@ -1,3 +1,60 @@
+// import React, { useEffect } from "react";
+// import AddIcon from "@mui/icons-material/Add";
+// import { Button } from "@mui/material";
+// import { useSelector, useDispatch } from "react-redux";
+// import { setPicture } from "../../../features/courseEditor/screenSlice";
+
+// export default function CloudinaryUploadWidget() {
+//   const dispatch = useDispatch();
+//   const screen = useSelector((state) => state.screenEditor.screen);
+//   const cloudName = process.env.REACT_APP_CLOUD_NAME;
+//   const uploadPreset = process.env.REACT_APP_UPLOAD_PRESET;
+
+//   useEffect(() => {
+//     const widget = window.cloudinary.createUploadWidget(
+//       {
+//         cloudName: cloudName,
+//         uploadPreset: uploadPreset,
+//         sources: [
+//           "local",
+//           "url",
+//           "camera",
+//           "image_search",
+//           "google_drive",
+//           "dropbox",
+//         ],
+//         googleApiKey: process.env.REACT_APP_GOOGLE_API_KEY,
+//         searchByRights: true,
+//       },
+//       (error, result) => {
+//         if (!error && result && result.event === "success") {
+//           dispatch(
+//             setPicture({ url: result.info.secure_url, screenId: screen._id })
+//           );
+//         }
+//       }
+//     );
+
+//     document.getElementById("upload_widget").addEventListener(
+//       "click",
+//       () => {
+//         widget.open();
+//       },
+//       false
+//     );
+//   }, [dispatch, screen._id, cloudName, uploadPreset]);
+
+//   return (
+//     <Button
+//       style={{ border: "1px solid #d9dddd" }}
+//       id='upload_widget'
+//       className='cloudinary-button'
+//     >
+//       <AddIcon />
+//       Bild hochladen
+//     </Button>
+//   );
+// }
 import React, { useEffect } from "react";
 import AddIcon from "@mui/icons-material/Add";
 import { Button } from "@mui/material";
@@ -10,48 +67,45 @@ export default function CloudinaryUploadWidget() {
   const cloudName = process.env.REACT_APP_CLOUD_NAME;
   const uploadPreset = process.env.REACT_APP_UPLOAD_PRESET;
 
-  useEffect(() => {
-    const widget = window.cloudinary.createUploadWidget(
-      {
-        cloudName: cloudName,
-        uploadPreset: uploadPreset,
-        sources: [
-          "local",
-          "url",
-          "camera",
-          "image_search",
-          "google_drive",
-          "dropbox",
-        ],
-        googleApiKey: process.env.REACT_APP_GOOGLE_API_KEY,
-        searchByRights: true,
-      },
-      (error, result) => {
-        if (!error && result && result.event === "success") {
-          dispatch(
-            setPicture({ url: result.info.secure_url, screenId: screen._id })
-          );
-        }
+  const widget = window.cloudinary.createUploadWidget(
+    {
+      cloudName: cloudName,
+      uploadPreset: uploadPreset,
+      sources: [
+        "local",
+        "url",
+        "camera",
+        "image_search",
+        "google_drive",
+        "dropbox",
+      ],
+      googleApiKey: process.env.REACT_APP_GOOGLE_API_KEY,
+      searchByRights: true,
+    },
+    (error, result) => {
+      if (!error && result && result.event === "success") {
+        dispatch(
+          setPicture({ url: result.info.secure_url, screenId: screen._id })
+        );
       }
-    );
+    }
+  );
 
-    document.getElementById("upload_widget").addEventListener(
-      "click",
-      () => {
-        widget.open();
-      },
-      false
-    );
-  }, [dispatch, screen._id, cloudName, uploadPreset]);
+  useEffect(() => {
+    document.getElementById("upload_widget").addEventListener("click", () => {
+      widget.open();
+    });
+  }, [widget]);
 
   return (
     <Button
       style={{ border: "1px solid #d9dddd" }}
-      id='upload_widget'
-      className='cloudinary-button'
+      id="upload_widget"
+      className="cloudinary-button"
     >
       <AddIcon />
       Bild hochladen
     </Button>
   );
 }
+
