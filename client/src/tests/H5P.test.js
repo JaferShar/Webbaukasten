00290a021f6446ecaef11780a59f99fcc5c2a50a
JaferShar <jafer.shar@gmail.com>
2023-03-04 19:02:53 +0100

    Test the H5P Page

---
+++ client/src/tests/H5P.test.js
@@ -0,0 +1,7 @@
+import { textTransform } from '@mui/system'
+import {render} from '@testing-library/react'
+import {Pixabay} from '../Components/Pixabay/PixabayOpen'
+
+test('Pixabay finds pictures with valid search-terms', () => {
+    render(<Pixabay />)
+})
