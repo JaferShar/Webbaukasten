b3c0a383ebac1b973b55e52bb37786f095ae6e31
JaferShar <jafer.shar@gmail.com>
2023-03-17 22:07:57 +0100

    client build für deployment

--- server/server.js
+++ server/server.js
@@ -1,15 +1,16 @@
-//const path = require('path');
 const express = require("express");
 const colors = require("colors");
 const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/MongoDBAccess");
+const path = require('path');
 const port = process.env.PORT || 5000;
 
 connectDB();
 
 const app = express();
-
+app.use(express.static(path.join(__dirname + "/public")))
+console.log(__dirname);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
